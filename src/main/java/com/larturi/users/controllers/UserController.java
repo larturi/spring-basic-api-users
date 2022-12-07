f0cbe2162724903d66157a3c6b3a4736994f7ac1
Leandro Arturi <lea.arturi@gmail.com>
2022-12-07 18:45:18 -0300

    getUserByUsername

--- src/main/java/com/larturi/users/controllers/UserController.java
+++ src/main/java/com/larturi/users/controllers/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +23,11 @@ public class UserController {
 	@GetMapping
 	public ResponseEntity<List<User>> getUsers() {
 		return new ResponseEntity<List<User>>(userService.getUsers(), HttpStatus.OK);
+	}
+	
+	@GetMapping(value="/{username}")
+	public ResponseEntity<User> getUserByUsername(@PathVariable("username") String username) {
+		return new ResponseEntity<User>(userService.getUserByUsername(username), HttpStatus.OK);
 	}
 
 }
--- src/main/java/com/larturi/users/services/UserService.java
+++ src/main/java/com/larturi/users/services/UserService.java
@@ -4,7 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.larturi.users.models.User;
 
@@ -23,14 +25,20 @@ public class UserService {
 	public List<User> getUsers() {
 		return users;
 	}
+	
+	public User getUserByUsername(String username) {
+		return users.stream().filter(u -> u.getUsername().equals(username)).findAny()
+			.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, String.format("User %s not found", username)));
+	}
 
 	@PostConstruct
 	public void init() {
 		for (int i = 0; i < 100; i++) {
 			users.add(new User(
-					faker.funnyName().name(), 
-					faker.name().username(), 
-					faker.dragonBall().character()));
+					faker.name().username(),
+					faker.name().name(), 
+					faker.dragonBall().character()
+					));
 			
 		}
 	}
